8c460bc00cfc0101d74869b01691e109b651a3d7
anhtrietrop <146307502+anhtrietrop@users.noreply.github.com>
2024-11-19 10:07:02 +0700

    123
    123

---
+++ BLL/FrmChonMonBLL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Text.RegularExpressions;
+using DTO;
+using DAL;
+
+namespace BLL
+{
+    public class FrmChonMonBLL
+    {
+        private DAL.FrmChonMonDAL frmChonMonDAL;
+        public FrmChonMonBLL()
+        {
+            frmChonMonDAL = new FrmChonMonDAL();
+        }
+        public DataTable GetMenu()
+        {
+            return frmChonMonDAL.GetMenu();
+        }
+    }
+}
---
+++ BLL/NhanVien.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Text.RegularExpressions;
+using DTO;
+using DAL;
+
+namespace BLL
+{
+    public class NhanVien
+    {
+        public bool KiemTraThongTinHopLe(string tenNV, string diaChi, string sdt, string email, string dangNhap, string matKhau)
+        {
+            if(string.IsNullOrWhiteSpace(tenNV)) return false;
+            if(!Regex.IsMatch(sdt, @"^\d{10,15}$")) return false;
+            if(!Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$")) return false;
+            if(string.IsNullOrWhiteSpace(dangNhap) || string.IsNullOrWhiteSpace(matKhau)) return false;
+            return true;
+        }
+        private DAL.NhanVien nhanVienDAL = new DAL.NhanVien();
+        public DataTable TimNhanVien(string maNV, string tenNV)
+        {
+            if (string.IsNullOrWhiteSpace(maNV) && string.IsNullOrWhiteSpace(tenNV))
+            {
+                throw new ArgumentException("Từ khóa tìm kiếm không được để trống!");
+            }
+
+            return nhanVienDAL.TimNhanVien(maNV, tenNV);
+        }
+    }
+}
