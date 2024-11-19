fb4afa0b60662c1490b41d620a85221794344516
anhtrietrop <146307502+anhtrietrop@users.noreply.github.com>
2024-11-19 10:08:48 +0700

    1
    1

---
+++ DTO/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    internal class Menu
+    {         
+        private string MaThucDon { get; set; }
+        private string TenThucDon { get; set;}
+        private float Gia {  get; set; }
+    }
+}
---
+++ DTO/NhanVien.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class NhanVien
+    {
+        private int MaNhanVien { get; set; }
+        private string TenNV { get; set; }
+        private string DiaChi { get; set; }
+        private string SDT {  get; set; }
+        private string Email { get; set; }
+        private string TenDangNhap { get; set; }
+        private string MatKhau { get; set; }
+    }
+}
