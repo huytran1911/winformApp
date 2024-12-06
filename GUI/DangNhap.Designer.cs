d54a942f96e248b1ef9cb3d4b25121102e0914be
khaivan123 <khaivp.32@gmail.com>
2024-12-06 13:54:08 +0700

    Cập nhật .gitignore để loại bỏ tệp không cần thiết

--- GUI/DangNhap.Designer.cs
+++ GUI/DangNhap.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            pictureBox1 = new PictureBox();
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
@@ -37,18 +36,8 @@
             txtMatKhau = new TextBox();
             btnDangNhap = new Button();
             btnThoat = new Button();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+
             SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Image = Properties.Resources.T1;
-            pictureBox1.Location = new Point(0, 0);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(419, 451);
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
             // 
             // label1
             // 
